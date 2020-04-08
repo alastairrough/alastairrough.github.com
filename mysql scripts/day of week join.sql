34ea9b63ae5973c6615533a8f3d5e5c3993ea954
Alastair Rough <rough.alastair@gmail.commit>
2020-04-08 09:58:56 -0700

    update

--- mysql scripts/day of week join.sql
+++
@@ -1,2 +0,0 @@
-use sitepoint;
-select a.dayofWeek, b.dayofWeek, a.DoW from dayofweekLookup a, SailingLoads b where a.dayofWeek = b.dayofWeek 
--- mysql scripts/last-few-days.sql
+++
@@ -1,3 +0,0 @@
-use sitepoint;
-select * from SailingLoadsJan2 where CaptureDate >= (DATE(NOW()) - INTERVAL 7 DAY) order by CaptureDate;
-
